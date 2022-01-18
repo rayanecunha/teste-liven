29f60407459c92c28cca798a02a74615ab8856a2
Rayane Gomes <rayane.gomes@jussi.local>
2022-01-18 17:23:51 -0300

    feat: add get products in server side render

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,46 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import { GetServerSideProps } from 'next'
+import { getProducts } from "../services/api"
 
-export default function Home({
-  allPostsData
-}: {
-  allPostsData: {
-    date: string
-    title: string
-    id: string
+type ProductsProps = {
+  products: {
+    price: string;
+    name: string;
+    image: string;
+    stock: number;
   }[]
-}) {
+}
+
+export default function Home({ products }: ProductsProps) {
   return (
     <Layout>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      
+      {products?.map(item => 
+        <div>
+          <img src={item?.image}/>
+          <p>{item?.name}</p>
+          <p>{item?.price}</p>
+          <p>{item?.stock}</p>
+        </div>
+      )}
     </Layout>
   )
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
+  const data = await getProducts().then(response => {
+    return response.json().then(data => ({
+      data: data,
+      status: response.status
+    }));
+  }).then(res => res.data);
+
   return {
     props: {
-      
+      products: data
     }
   }
 }
