bd9af1ac31bc60276c95bb72266dda5ed9cf2e98
Rayane Gomes <rayane.gomes@jussi.local>
2022-01-21 09:33:11 -0300

    feat: add products in home page

--- components/Shelf/index.tsx
+++ components/Shelf/index.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useEffect, useContext } from 'react'
 import { ShelfList } from './styles'
 import { ProductsProps } from '../../pages'
 import Card from "../Card"
+import AppContext from "../../context/appContext"
 
 const Shelf = ({ products }: ProductsProps) => {
+    const appContext = useContext(AppContext)
+    const { getCartProducts, addCartProducts } = appContext
+
+    useEffect(() => {
+        getCartProducts()
+    }, [])
+
     return <ShelfList>
         {products?.map((item, index) => 
-            <Card product={item} key={index} />
+            <Card product={item} key={index} handleEvent={() => addCartProducts(item)} />
       )}
     </ShelfList>   
 }
